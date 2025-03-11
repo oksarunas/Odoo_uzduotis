cba36baa9cfdb7d9bbc04780e0aad0906b727b97
Sarunas <sarunas.karp@gmail.com>
2025-03-11 18:39:01 +0200

    Initial setup

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Equipment Register',
+    'version': '1.0',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+}
