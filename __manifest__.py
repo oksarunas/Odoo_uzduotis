f193395c91e3bed80c4fa13958359a412e6b66f9
Sarunas <sarunas.karp@gmail.com>
2025-03-13 22:42:17 +0200

    Prideta vartotojo sasaja

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,12 @@
     'version': '1.0',
     'category': 'Tools',
     'depends': ['base'],
-    'data': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/equipment_security.xml',
+        'views/equipment_views.xml',
+        'wizard/equipment_filter_views.xml'
+    ],
     'installable': True,
     'auto_install': False,
 }
