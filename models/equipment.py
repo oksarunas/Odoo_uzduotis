8d8995a365800d13e5a1462faed85c9da753994d
Sarunas <sarunas.karp@gmail.com>
2025-03-13 22:32:55 +0200

    Pakeistas description equipment.py faile

--- models/equipment.py
+++ models/equipment.py
@@ -6,7 +6,7 @@ class Equipment(models.Model):
 
     name = fields.Char(string='Įrangos pavadinimas', required=True)
     serial_number = fields.Char(string='Unikalus įrangos identifikatorius', required=True)
-    description = fields.Text(string='Description')
+    description = fields.Text(string='Papildoma informacija apie įrangą')
     company_id = fields.Many2one('res.company', string='Įrangą valdanti įmonė', default=lambda self: self.env.company)
     create_uid = fields.Many2one('res.users', string='Sukūręs vartotojas', default=lambda self: self.env.user, readonly=True)
     employee_ids = fields.Many2many('res.users', string='Atsakingi darbuotojai')
