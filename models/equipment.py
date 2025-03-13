f5d0a110af9c8947d0d0f6ef48bb3da59ee7dc94
Sarunas <sarunas.karp@gmail.com>
2025-03-13 22:29:17 +0200

    Atnaujinti pavadinimai models.py

--- models/equipment.py
+++ models/equipment.py
@@ -4,9 +4,10 @@ class Equipment(models.Model):
     _name = 'equipment.register'
     _description = 'Equipment Register'
 
-    name = fields.Char(string='Name', required=True)
-    serial_number = fields.Char(string='Serial Number', required=True)
+    name = fields.Char(string='Įrangos pavadinimas', required=True)
+    serial_number = fields.Char(string='Unikalus įrangos identifikatorius', required=True)
     description = fields.Text(string='Description')
-    company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company)
-    create_uid = fields.Many2one('res.users', string='Created By', default=lambda self: self.env.user, readonly=True)
-    employee_ids = fields.Many2many('res.users', string='Responsible Employees')
+    company_id = fields.Many2one('res.company', string='Įrangą valdanti įmonė', default=lambda self: self.env.company)
+    create_uid = fields.Many2one('res.users', string='Sukūręs vartotojas', default=lambda self: self.env.user, readonly=True)
+    employee_ids = fields.Many2many('res.users', string='Atsakingi darbuotojai')
+    acquisition_date = fields.Date(string= 'Įsigijimo data', default=fields.Date.today)
